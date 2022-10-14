fac41f4232b8a4365c3a251e876fc7bb5d1d4051
mazurasdf <mazurasdf@gmail.com>
2022-10-14 11:08:08 -0500

    day 5 algo code

---
+++ algos/d5.js
@@ -0,0 +1,33 @@
+// var fruit1 = "apples";
+// var fruit2 = "oranges";
+
+// fruit1 = fruit2;
+// fruit2 = fruit1;
+
+// console.log("My favorite fruit is " + fruit1);
+// console.log("My second favorite fruit is " + fruit2);
+
+// var fruit1 = "apples";
+// var fruit2 = "oranges";
+
+// var temp = fruit1;
+// fruit1 = fruit2;
+// fruit2 = temp;
+
+// console.log("My favorite fruit is " + fruit1);
+// console.log("My second favorite fruit is " + fruit2);
+
+// var start = 0;
+// var end = 12;
+// while(start < end){
+//     console.log(`start: ${start}, end: ${end}`);
+//     start += 2;
+//     end -= 2;
+// }
+
+function reverse(arr){
+    //your code here!
+}
+
+var letters = ["a","b","c","d","e"];
+console.log(reverse(letters));//should print out ["e","d","c","b","a"]
